c2409dea6b58cde5d896cceaadd6153238c0f53a
antohimarian96 <marian_antohi96@yahoo.com>
2019-02-04 19:28:23 +0200

    Program

---
+++ FootballClub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_TextValidationWithClasses
+{
+    class FootballClub
+    {
+        private readonly string name;
+        private readonly int points;
+
+        public FootballClub(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public int Points()
+        {
+            return points;
+        }
+
+        public string Name()
+        {
+            return name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSON_TextValidationWithClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
