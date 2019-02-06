9ecb238de52b3cbc416d4a1995a4917f0439a19a
antohimarian96 <46729207+antohimarian96@users.noreply.github.com>
2019-02-06 10:36:21 +0200

    Add files via upload

--- FootballClub.cs
+++ FootballClub.cs
@@ -2,27 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace JSON_TextValidationWithClasses
+namespace FootballRankingWithClasses
 {
     class FootballClub
     {
-        private readonly string name;
-        private readonly int points;
-
-        public FootballClub(string name, int points)
-        {
-            this.name = name;
-            this.points = points;
-        }
-
-        public int Points()
-        {
-            return points;
-        }
-
-        public string Name()
-        {
-            return name;
-        }
     }
 }
--- FootballRanking.cs
+++ FootballRanking.cs
@@ -2,50 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace JSON_TextValidationWithClasses
+namespace FootballRankingWithClasses
 {
     class FootballRanking
     {
-        private FootballClub[] club;
-
-        public FootballRanking()
-        {
-            for (int i = 0; i < 14; i++)
-            {
-                string[] clubDetails = Console.ReadLine().Split('-');
-                club[i] = new FootballClub(clubDetails[0], Convert.ToInt32(clubDetails[1]) + Convert.ToInt32(clubDetails[2]));
-            }
-        }
-
-        public FootballClub[] SortFootballRanking(FootballClub[] club)
-        {
-            FootballClub aux;
-            int length = 14;
-            for (int i = 0; i < length; i++)
-            {
-                for (int j = 0; j < length - 1; j++)
-                {
-                    if (club[j].Points() < club[j + 1].Points())
-                    {
-                        CheckIfSwapingPosition(j, j + 1, club[j].Name(), club[j + 1].Name());
-                        aux = club[j];
-                        club[j] = club[j + 1];
-                        club[j + 1] = aux;
-                    }
-                }
-            }
-            return club;
-        }
-
-        private void CheckIfSwapingPosition(int index, int nextIndex,string firstClubName, string secondClubName)
-        {
-            Console.WriteLine("Swapping elements with indexes (" + index + ", " + nextIndex + ") and values (" + firstClubName + ", " + secondClubName + ")");
-        }
-
-        private static void ShowFootballRankingTable(FootballClub[] footballClubs)
-        {
-            for (int i = 0; i < footballClubs.Length; i++)
-                Console.WriteLine(footballClubs[i].Name() + "- " + footballClubs[i].Points());
-        }
     }
 }
